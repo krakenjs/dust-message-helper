13be3843c069f133d5d4dbcdb56f7b884e96fa56
Aria Stewart <aredridel@dinhe.net>
2014-09-16 09:59:26 -0700

    Make it so

---
+++ index.js
@@ -0,0 +1,6 @@
+var dust = require('dustjs-linkedin');
+require('dustjs-helpers');
+
+module.exports = dust.helpers.pre = dust.helpers.message = function message(chunk, ctx, bodies, params) {
+    return chunk.write(ctx.get('messages')[dust.helpers.tap(params.key, chunk, ctx)]);
+};
