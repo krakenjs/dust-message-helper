7a4012ef37cb7c5c48f1c534cfacd7760d9bf907
Aria Stewart <aredridel@dinhe.net>
2014-09-18 16:12:45 -0700

    Get rid of dustjs-helpers

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
 var dust = require('dustjs-linkedin');
-require('dustjs-helpers');
 
 module.exports = dust.helpers.pre = dust.helpers.message = function message(chunk, ctx, bodies, params) {
-    return chunk.write(ctx.get('messages')[dust.helpers.tap(params.key, chunk, ctx)]);
+    var msg = (ctx.get('messages') || {})[params.key];
+    return chunk.write(msg);
 };
