8c9fd60183fde4d474a6589760eb6b3c81180390
Aria Stewart <aredridel@dinhe.net>
2014-09-18 16:30:14 -0700

    Add support for mode=json

--- index.js
+++ index.js
@@ -4,5 +4,10 @@ var dust = require('dustjs-linkedin');
 
 module.exports = dust.helpers.pre = dust.helpers.message = function message(chunk, ctx, bodies, params) {
     var msg = (ctx.get('messages') || {})[params.key] || '☃' + params.key + '☃';
-    return chunk.write(msg);
+
+    if (params.mode == 'json') {
+        return chunk.write(JSON.stringify(msg));
+    } else {
+        return chunk.write(msg);
+    }
 };
