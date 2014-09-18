c19d6c940d5511537a81a743763c422aa28618d6
Aria Stewart <aredridel@dinhe.net>
2014-09-18 16:13:43 -0700

    Do you want to build a snowman?

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 var dust = require('dustjs-linkedin');
 
 module.exports = dust.helpers.pre = dust.helpers.message = function message(chunk, ctx, bodies, params) {
-    var msg = (ctx.get('messages') || {})[params.key];
+    var msg = (ctx.get('messages') || {})[params.key] || '☃' + params.key + '☃';
     return chunk.write(msg);
 };
