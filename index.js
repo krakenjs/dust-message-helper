91e8c747a192c4100ddc6210a2dfdcdfad043eeb
Aria Stewart <aredridel@dinhe.net>
2014-10-02 14:34:40 -0400

    Run content back through dust so variables work

--- index.js
+++ index.js
@@ -40,7 +40,13 @@ module.exports = dust.helpers.pre = dust.helpers.message = function message(chun
         value = String(value);
     }
 
-    chunk.write(value);
+    /* And thus begins the ugly, possibly expensive hack to run dynamically loaded content through Dust */
+    var cacheKey = 'content for ' + ctx.templateName + ' key ' + params.key;
+    if (!dust.cache[cacheKey]) {
+        dust.loadSource(dust.compile(value, cacheKey));
+    }
+    dust.cache[cacheKey](chunk, ctx);
+    /* Here endeth the confusion, on Setting Orange, the 56th day of Bureaucracy in the YOLD 3180 */
 
     return chunk;
 
