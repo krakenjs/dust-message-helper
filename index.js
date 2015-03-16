02398dbbf7d4e6f068eccd5e9a2b0d1ec45e5fc5
Aria Stewart <aredridel@dinhe.net>
2015-03-16 14:04:31 -0400

    Add another chunk indirection since we cannot know what context we were called in

--- index.js
+++ index.js
@@ -39,16 +39,15 @@ module.exports = function (dust) {
             value = String(value);
         }
 
-        /* And thus begins the ugly, possibly expensive hack to run dynamically loaded content through Dust */
-        var cacheKey = ctx.templateName + params.key + encodeURI(value).replace(/%/g, '_');
-        if (!dust.cache[cacheKey]) {
-            dust.loadSource(dust.compile(value, cacheKey));
-        }
-        dust.cache[cacheKey](chunk, ctx);
-        /* Here endeth the confusion, on Setting Orange, the 56th day of Bureaucracy in the YOLD 3180 */
-
-        return chunk;
-
+        return chunk.map(function (chunk) {
+            /* And thus begins the ugly, possibly expensive hack to run dynamically loaded content through Dust */
+            var cacheKey = ctx.templateName + params.key + encodeURI(value).replace(/%/g, '_');
+            if (!dust.cache[cacheKey]) {
+                dust.loadSource(dust.compile(value, cacheKey));
+            }
+            dust.cache[cacheKey](chunk, ctx).end();
+            /* Here endeth the confusion, on Setting Orange, the 56th day of Bureaucracy in the YOLD 3180 */
+        });
     };
 };
 
